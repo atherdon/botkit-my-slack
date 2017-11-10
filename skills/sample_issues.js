7ae4acf435e09b34656a0b812786edd632cd9f16
Glitch (imported-crayon) <none>
2017-11-10 04:56:59 +0000

    📖😎 Updated with Glitch

--- skills/sample_issues.js
+++ skills/sample_issues.js
@@ -49,7 +49,7 @@ module.exports = function(controller) {
   
       // listen for a user saying "add <something>", and then add it to the user's list
     // store the new list in the storage system
-    controller.hears(['addi (.*)'],'direct_message,direct_mention,mention', function(bot, message) {
+    controller.hears(['add (.*)'],'direct_message,direct_mention,mention', function(bot, message) {
 
         var newtask = message.match[1];
         controller.storage.users.get(message.user, function(err, user) {
@@ -87,6 +87,7 @@ module.exports = function(controller) {
           'cutstream-api',
           'stripe-recurring-membership'
         ];
+      
       console.log(list)
         let attachments = [
         { 
