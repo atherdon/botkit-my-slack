e2488737817a7cd5e219cc02e188ef5edc76ec33
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-12-20 18:07:21 +0300

    +

---
+++ data/main_array.js
@@ -0,0 +1,42 @@
+
+const _ = require('underscore');
+
+
+
+//------------
+// shopping
+let view = [ date, ingredient_name, department_name ];
+
+// ideas
+let view = [ title, priority, monetization ];
+
+// cookbook
+let view = [ recipe_title, recipe_image, order_sort ];
+
+// restaurant
+let view = [ menu_name, {category_name, { dishes_list }} ];
+
+// dietary
+let view = [ month, actions, department_name ];
+let view2 = [ calories, {cooking_list_name, {calorie, item}} ];
+
+// Expenses
+let view = [ month, {sum_list} ];
+
+// video
+let view = [ date, who_send, link_to_browser];
+
+// recipes
+let view = [ id, title, description, steps, date ];
+
+// search
+let view = [ link, items, ULR ];
+
+// fitness
+let view  = [ date, {calories_list} ];
+let view2 = [ type_name, {date, result, calories_burn} ];
+
+// measurement
+let view  = [ name, plurals ]; //[kg, g, ml]
+let view2 = [ how_much_items_we_have(sum), sum_of_ingredients ]; // [3, 1,6 kg, salt]
+let view3 = [name, {list of converted sub_names}]
