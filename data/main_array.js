784dc2223e41a3fd01f74b8ce2d87de6e6dda51b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-12-24 10:11:00 +0300

    +

--- data/main_array.js
+++ data/main_array.js
@@ -6,9 +6,12 @@ const _ = require('underscore');
 //------------
 // Shopping
 let view = [ date, ingredient_name, department_name ];
+[gl_id, dep_id, ingredient_name]
+"gl_id": {ingredient_name, department_name}
 
 // Ideas
 let view = [ title, priority, monetization ];
+"ideas": {id, date, title, monetization -> yes or no, priority}
 
 // Cookbook
 let view = [ recipe_title, recipe_image, order_sort ];
@@ -19,22 +22,109 @@ let view = [ menu_name, {category_name, { dishes_list }} ];
 // Dietary
 let view = [ month, actions, department_name ];
 let view2 = [ calories, {cooking_list_name, {calorie, item}} ];
+"calendar":
+
+[
+    {
+        "id":"123",
+        "title":"foo1",
+        "start":"2016-02-12T10:30:00",
+        "end":"2016-02-12T12:30:00"
+    },
+
+    {
+        "id":"456",
+        "title":"foo2",
+        "start":"2016-02-14T10:30:00",
+        "end":"2016-02-14T12:30:00"
+    }
+];
+
+
+
+    "properties": {
+        "dtstart": {
+            "format": "date-time",
+            "type": "string",
+            "description": "Event starting time"
+        },
+        "dtend": {
+            "format": "date-time",
+            "type": "string",
+            "description": "Event ending time"
+        },
+        "summary": { "type": "string" },
+        "location": { "type": "string" },
+        "url": { "type": "string", "format": "uri" },
+        "duration": {
+            "format": "time",
+            "type": "string",
+            "description": "Event duration"
+        },
+        "rdate": {
+            "format": "date-time",
+            "type": "string",
+            "description": "Recurrence date"
+        },
+
+        "category": { "type": "string" },
+        "description": { "type": "string" },
+      }
+
+
+"month": [{date, action, calories}]
+
+
 
 // Expenses
 let view = [ month, {sum_list} ];
+{
+  month name: [ [sum, date],[sum, date] ]
+};
+{
+  payment types: ["beer", "apartment", "glow", "wifi"]
+};
+
 
 // Video
 let view = [ date, who_send, link_to_browser];
+[id, date, url, time short, who send]
+
 
 // Recipes
 let view = [ id, title, description, steps, date ];
+ingredients with [ingredient, calories]
 
 // Search
 let view = [ link, items, ULR ];
 
+{
+  ingredients: [A1, B1, C1, D1]
+};
+{
+  categories: [2, 21, 22]
+};
+{
+  cousine: [A, B, C]
+};
+{
+  cook times: [0.5 hour, 1 hour, 10 mins]
+};
+{
+  small settings: [{name, value}, {name, value}]
+};
+
 // Fitness
 let view  = [ date, {calories_list} ];
 let view2 = [ type_name, {date, result, calories_burn} ];
+
+"jogging": [{date, how much, calories}, {date, how much, calories}]
+"hike": [{date, time, calories}]
+"joga": [{date, yes or no}]
+"weight": [{date, time, amount}]
+"workout": [{date, time, yes or no}]
+"steps"
+"calories"
 
 // Measurement
 let view  = [ name, plurals ]; //[kg, g, ml]
