3023ff0e62fdb1589099e856986fb81bfc198092
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-12-27 07:05:04 +0300

    +++

--- readme-help-restaurnat.md
+++ readme-help-restaurnat.md
@@ -8,7 +8,7 @@ create restaurant menu from slack
 List of commands (just type help in the channel)
 
 * `help` : this list
-* `menu` : list current track
+* `menu` : list current menu items
 * `menu` _type_ : create menu items in this category - maybe????
 * `item` _price_, _category_ : Add item into menu with price and category.
 * `add` _name_, _measurement_, _price_ : Add item with measurements, like "salad 350g", like "long island 300ml"
--- skills/help.js
+++ skills/help.js
@@ -1,11 +1,23 @@
-
+//
+// let help_bugbot = 'You can say these things to me(just type help in the channel):\n'+
+//     '\tList of commands\n'+
+//     '\t⦿ \n'+
+//     '\t⦿ \n'+
+//     '\t⦿ \n'+
+//     '\t⦿ \n'+
+//     '\t⦿ \n'+
+//     '\t⦿ \n'+
+//     '\t⦿ \n'+
+//     '\t⦿ \n'+
+//     '\t⦿ \n'
+//     ;
 
 let help_bugbot = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
+    '\t⦿ `add` : add an issue\n'+
+    '\t⦿ `auth` : login\n'+
+    '\t⦿ `issues` : list of all issues\n'+
     '\t⦿ `logout` : logout from all\n'+
     '\t⦿ `repo` pick : pick a repository to work with\n'+
     '\t⦿ `repos` : list of all repositories\n'+
@@ -13,241 +25,157 @@ let help_bugbot = 'You can say these things to me(just type help in the channel)
     '\t⦿ `search` : list of all repositories\n'
     ;
 
-    * `help` : this list
-    * `recipe` add: add a new recipe into your cookbook
-    * `view` _cookbookName_ : search for a cookbook, display data in a proper way
 
 let help_cookbook = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `recipe` add: add a new recipe into your cookbook\n'+
+    '\t⦿ `view` _cookbookName_ : search for a cookbook, display data in a proper way\n'
     ;
 
-    * `help` : this list
-    * `calendar` : manage your diet calendar
-    * `calendar` _add_ : add new item
-    * `calendar` _view_ : view diet calendar - current week only
-    * `create` _dietName_ : create a new diet
-    * `manage` _dietName_ : manage your diet
-    * `calories` : list of your eaten calories
-    * `calories` _add_ : add a new meal with calories
-    * `ingredients` : View list of ingredients of your diet
-    * `steps` : or description - it's like a step-by-step plan what you must to do or what goal you must an accomplish
 
 let help_dietary = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `calendar` : manage your diet calendar\n'+
+    '\t⦿ `calendar` _add_ : add new item\n'+
+    '\t⦿ `calendar` _view_ : view diet calendar - current week only\n'+
+    '\t⦿ `create` _dietName_ : create a new diet\n'+
+    '\t⦿ `manage` _dietName_ : manage your diet\n'+
+    '\t⦿ `calories` : list of your eaten calories\n'+
+    '\t⦿ `calories` _add_ : add a new meal with calories\n'+
+    '\t⦿ `ingredients` : View list of ingredients of your diet\n'+
+    '\t⦿ `steps` : or description - it\'s like a step-by-step plan what you must to do or what goal you must an accomplish\n'
     ;
 
-    * `help` : this list
-    * `add` : add a new purchased item
-    * `view` _name_ : view details about purchased item
-    * `view` list : view list of purchased items
-    * `delete` _nameOrNumber_ : Delete item from the list
-    * `ingredients`: Display list of purchsed ingredients Can be converted later to /items
-    * `beer` : Track how much beer did you buy
-    * `apartment` : Add your monthly apartment costs
-    * `wifi`  : Add your monthly internet costs
-    * `glowing` : add how much you spend on this month on electricity
-    * `calendar` : Calendar view with your must-to-purhase or pay stuff
-    * `grid` : display stuff in a grid view
-    * `view` _date_: view by date
-    * `add` prices: just track prices via comma
-    * `add` price: [name, sum, date] format - add single purchased item
-    * `calculate` _dateOrMonth_: How much did you spend on date or by month
 
 let help_expenses = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `add` : add a new purchased item\n'+
+    '\t⦿ `view` _name_ : view details about purchased item\n'+
+    '\t⦿ `view` list : view list of purchased items\n'+
+    '\t⦿ `delete` _nameOrNumber_ : Delete item from the list\n'+
+    '\t⦿ `ingredients`: Display list of purchsed ingredients Can be converted later to /items\n'+
+    '\t⦿ `beer` : Track how much beer did you buy\n'+
+    '\t⦿ `apartment` : Add your monthly apartment costs\n'+
+    '\t⦿ `wifi`  : Add your monthly internet costs\n'+
+    '\t⦿ `glowing` : add how much you spend on this month on electricity\n'
+    '\t⦿ `calendar` : Calendar view with your must-to-purhase or pay stuff\n'
+    '\t⦿ `grid` : display stuff in a grid view\n'
+    '\t⦿ `view` _date_: view by date\n'
+    '\t⦿ `add` prices: just track prices via comma\n'
+    '\t⦿ `add` price: [name, sum, date] format - add single purchased item\n'
+    '\t⦿ `calculate` _dateOrMonth_: How much did you spend on date or by month\n'
     ;
 
-    * `help` : this list
-    * `add` _purchase_: add a new item into list
-    * `view` : view all purchases
-    * `view` _type_ : view purchases by type
-    * `calculate`: Calculate something
 
 let help_family_budget = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `add` _purchase_: add a new item into list\n'+
+    '\t⦿ `view` : view all purchases\n'+
+    '\t⦿ `view` _type_ : view purchases by type\n'+
+    '\t⦿ `calculate`: Calculate something\n'
     ;
 
-    * `help` : this list
-    * `jogging` _text_: add your new train
-    * `hike` _text_ : add your new train
-    * `workout` _text_ : add your new train
-    * `yoga` _text_ : add your new train
-    * `log` : what we can check with this? food log?
-    * `view` _categoryName_: display data, related to some of category
-    * `view` all: display all data, pushed in some last days. maybe week
-    * `weight` _text_: Track your weight
-    * `calories` _text_: Track your weight
-    * `steps` _text_: Track your weight
 
 let help_fitness = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `jogging` _text_: add your new train\n'+
+    '\t⦿ `hike` _text_ : add your new train\n'+
+    '\t⦿ `workout` _text_ : add your new train\n'+
+    '\t⦿ `yoga` _text_ : add your new train\n'+
+    '\t⦿ `log` : what we can check with this? food log?\n'+
+    '\t⦿ `view` _categoryName_: display data, related to some of category\n'+
+    '\t⦿ `view` all: display all data, pushed in some last days. maybe week\n'+
+    '\t⦿ `weight` _text_: Track your weight\n'+
+    '\t⦿ `calories` _text_: Track your weight\n'
+    '\t⦿ `steps` _text_: Track your weight\n'
     ;
-
-    * `help` : this list
-    * `add` _title_, _description_, _priority_, _monetization_ : add an idea. Monetization is yes/no question
-    * `push` _repository_ : push to repo
 
 let help_idea = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `add` _title_, _description_, _priority_, _monetization_ : add an idea. Monetization is yes/no question\n'+
+    '\t⦿ `push` _repository_ : push to repo\n'
     ;
 
-    * `help` : this list
-    * `add` : add a new measurement into database
-    * `view` _type_ : search by type and display a list or related items (1 kg is)
-    * `view` all : display all information that we have
-    * `convertation`: Convert from one into another
-    * `multiplication` _values_ :  _values__ == "1kg, 0.5kg" So we need to count how much we eat sugar per year. is 1.5k
+    *
+    *
+    *
+    *
+    *
 
 let help_measurements = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `add` : add a new measurement into database\n'+
+    '\t⦿ `view` _type_ : search by type and display a list or related items (1 kg is)\n'+
+    '\t⦿ `view` all : display all information that we have\n'+
+    '\t⦿ `convertation`: Convert from one into another\n'+
+    '\t⦿ `multiplication` _values_ :  _values__ == "1kg, 0.5kg"\n'
     ;
 
-    * `help` : this list
-    * `view` all : display all added recipes
-    * `view` one : display only one recipe - by name???
-    * `view` date : display by date
-    * `calendar` : calendar with what???
-    * `preview` : preview recipes, maybe? for what reason???
+    *
+    *
+    *
+    *
+    *
 
 let help_recipe_add = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `view` all : display all added recipes\n'+
+    '\t⦿ `view` one : display only one recipe - by name???\n'+
+    '\t⦿ `view` date : display by date\n'+
+    '\t⦿ `calendar` : calendar with what???\n'+
+    '\t⦿ `preview` : preview recipes, maybe? for what reason???\n'
     ;
 
-    * `help` : this list
-    * `menu` : list current track
-    * `menu` _type_ : create menu items in this category - maybe????
-    * `item` _price_, _category_ : Add item into menu with price and category.
-    * `add` _name_, _measurement_, _price_ : Add item with measurements, like "salad 350g", like "long island 300ml"
-    * `preview` : check how it looks
-    * `list` : list view
-    * `grid` : grid view
+
+
+let shopping_list = 'You can say these things to me(just type help in the channel):\n'+
+    '\tList of commands\n'+
+    '\t⦿ `help` : this list\n'+
+    '\t⦿ `directions` : list with directions from your current list\n'+
+    '\t⦿ `view` _directionName_ : search for a direction, display ingredients/items in list view\n'+
+    '\t⦿ `groceries` : Display list of your saved groceries\n'+
+    '\t⦿ `add` _groceryListName_ : Create a new grocery list\n'+
+    '\t⦿ `clone` _groceryIdOrName_ : Will create a new version of grocery list by cloning other list\n'+
+    '\t⦿ `list` _category_ : List of ingredients by category\n'+
+    '\t⦿ `delete` _number_ : remove ingredient from grocery list\n'+
+    '\t⦿ `purchase` _number_ : make an ingredient purchased\n'+
+    '\t⦿ `share` _groceryListId_ : share current GL with other people\n'
+    ;
+
 
 let help_restaurant_menu = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `menu` : list current items\n'+
+    '\t⦿ `menu` _type_ : create menu items in this category - maybe????\n'+
+    '\t⦿ `item` _price_, _category_ : Add item into menu with price and category.\n'+
+    '\t⦿ `add` _name_, _measurement_, _price_ : Add item with measurements, like "salad 350g", like "long island 300ml"\n'+
+    '\t⦿ `preview` : check how it looks\n'+
+    '\t⦿ `list` : list view\n'+
+    '\t⦿ `grid` : grid view\n'
     ;
 
-    * `help` : this list
-    * `menu` : list current track
-    * `menu` _type_ : create menu items in this category - maybe????
-    * `item` _price_, _category_ : Add item into menu with price and category.
-    * `add` _name_, _measurement_, _price_ : Add item with measurements, like "salad 350g", like "long island 300ml"
-    * `preview` : check how it looks
-    * `list` : list view
-    * `grid` : grid view
 
-let help_shopping_list = 'You can say these things to me(just type help in the channel):\n'+
-    '\tList of commands\n'+
-    '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
-    ;
-
-    * `help` : this list
-    * `view` _nameOrNumber_ : search and display video
-    * `view` all : display list with all saved videos
-    * `add` _text_ : Add video
-    * `add` completed : video marked as watched
-    * `view` _onlyNewVideos_ : see fresh first
 
 let help_video = 'You can say these things to me(just type help in the channel):\n'+
     '\tList of commands\n'+
     '\t⦿ `help` : this list\n'+
-    '\t⦿ * `add` : add an issue\n'+
-    '\t⦿ * `auth` : login\n'+
-    '\t⦿ * `issues` : list of all issues\n'+
-    '\t⦿ `logout` : logout from all\n'+
-    '\t⦿ `repo` pick : pick a repository to work with\n'+
-    '\t⦿ `repos` : list of all repositories\n'+
-    '\t⦿ `alias` : list of all repositories\n'+
-    '\t⦿ `search` : list of all repositories\n'
+    '\t⦿ `view` _nameOrNumber_ : search and display video\n'+
+    '\t⦿ `view` all : display list with all saved videos\n'+
+    '\t⦿ `add` _text_ : Add video\n'+
+    '\t⦿ `add` completed : video marked as watched\n'+
+    '\t⦿ `view` _onlyNewVideos_ : see fresh first\n'
     ;
+
+
 
 let array = [
       'You can say these things to me:',
