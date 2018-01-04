b685c7a593ea513db0b4fa38d28b58b832f165c3
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-01-04 12:25:14 +0300

    +

---
+++ data/help/family_budget.js
@@ -0,0 +1,6 @@
+//
+// * `help` : this list
+// * `add` _purchase_: add a new item into list
+// * `view` : view all purchases
+// * `view` _type_ : view purchases by type
+// * `calculate`: Calculate something
